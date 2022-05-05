87b9607af6a60f19deed1c136095eb7e35abb091
arieltecay <arieltecay@gmail.com>
2022-05-05 20:35:43 -0300

    Clase del 5 de mayo

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@
 const resultado = document.querySelector('#resultado')
 const year = document.querySelector("#year")
 const marca = document.querySelector('#marca')
+const minimo = document.querySelector("#minimo")
+const maximo = document.querySelector("#maximo")
+const puertas = document.querySelector("#puertas")
 
 const maxYear = new Date().getFullYear()
 const minYear = maxYear - 10
@@ -18,7 +21,8 @@ const datosBusqueda =
     marca: '',
     modelo: '',
     year: '',
-    precio: '',
+    minimo: 0,
+    maximo: 0,
     puertas: '',
     color: '',
     transmision: ''
@@ -28,6 +32,7 @@ const datosBusqueda =
 //Funciones
 function mostrarAutos(autos) {
     limpiarHTML()
+
     autos.forEach(auto => {
         const { marca, modelo, puertas, year, precio, color, transmision } = auto
 
@@ -54,10 +59,26 @@ function llenarSelect() {
 }
 
 function filtrarAuto() {
-    const resultado = autos.filter(filtrarMarca).filter(filtrarYear)
-    console.log(resultado);
-    mostrarAutos(resultado)
+    const resultado = autos.filter(filtrarMarca).filter(filtrarYear).filter(filtrarMinimo).filter(filtrarMaximo).filter(filtrarPuertas)
+    if (resultado.length) {
+        console.log(resultado);
+        mostrarAutos(resultado)
+    } else {
+        limpiarHTML()
+        noTieneAuto()
+    }
 }
+
+function noTieneAuto() {
+    const noResultado = document.createElement('div')
+    noResultado.classList.add('alerta', 'error')
+    noResultado.textContent = 'No Existen autos disponibles'
+    resultado.appendChild(noResultado)
+
+}
+/*     if (resultado === []) {
+        resultado.textContent("No se ha encontrado vehiculos con la información requerida");
+    } */
 
 function filtrarMarca(auto) {
     if (datosBusqueda.marca) {
@@ -73,6 +94,31 @@ function filtrarYear(auto) {
     return auto
 }
 
+function filtrarMinimo(auto) {
+    const { minimo } = datosBusqueda
+    if (minimo) {
+        return auto.precio >= minimo
+    }
+    return auto
+}
+
+function filtrarMaximo(auto) {
+    const { maximo } = datosBusqueda
+
+    if (maximo) {
+        return auto.precio <= maximo
+    }
+    return auto
+}
+
+function filtrarPuertas(auto) {
+    if (datosBusqueda.puertas) {
+        return auto.puertas === +datosBusqueda.puertas
+    }
+    return auto
+}
+
+
 //Listener
 
 marca.addEventListener('change', (e) => {
@@ -85,4 +131,20 @@ year.addEventListener('change', (e) => {
     datosBusqueda.year = e.target.value
     filtrarAuto()
 })
+minimo.addEventListener('change', (e) => {
+    /* console.log(e.target.value); */
+    datosBusqueda.minimo = e.target.value
+    filtrarAuto()
+})
 
+maximo.addEventListener('change', (e) => {
+    /* console.log(e.target.value); */
+    datosBusqueda.maximo = e.target.value
+    filtrarAuto()
+})
+
+puertas.addEventListener('change', (e) => {
+    /* console.log(e.target.value); */
+    datosBusqueda.puertas = e.target.value
+    filtrarAuto()
+})
